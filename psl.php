4ced6c4afea27a436bdad05c8a5378e97ab9521c
7666376872 <128346714+7666376872@users.noreply.github.com>
2023-07-12 11:39:25 +0530

    Add files via upload

---
+++ psl.php
@@ -0,0 +1,26 @@
+<?
+
+//echo"hello psl";
+//echo"pani pani pani uncleji mujhe pani piladijiye mera gala bohot sukh raha hai ";
+echo"ye buddha mere bich me bohot bolta hai";
+php
+// $num1 = 4;
+// echo $num1 * 6;
+// $num2 =50;
+// $num3 =19;
+// $sum =$num2 + $num3 ;
+// echo "<br>$num2 + $num3 = $sum";
+// "<br>"
+
+$x = 5;
+$y = 1;
+$temp=$x;
+$x = $y;
+$y = $temp;
+
+echo "<br> $temp";
+
+
+
+
+?>
